e2f4b0345e3c297e4ab7899d313ec4e393829aa0
nildiert <niljordan23@gmail.com>
2019-04-08 15:48:06 +0000

    [FIX] Changing the getline function for my own getline function

--- read_line.c
+++ read_line.c
@@ -1,24 +1,45 @@
 #include "shell.h"
+char _getchar()
+{
+  char c;
 
+  return (read(STDIN_FILENO, &c, 1) == 1) ? (unsigned char) c : EOF;
+}
 
 char *read_line(void)
 {
-  size_t linelen;
+  size_t linelen = 0;
   size_t buff_size = 1024;
+  char inp_char;
   char line[1024];
   char *lptr = line;
 
   
   (void)linelen;
-  write(STDOUT_FILENO, "$", 1);
-  write(STDOUT_FILENO, " ", 1);  
-  linelen = getline(&lptr, &buff_size, stdin);
-  if (lptr[linelen - 1] == EOF)
-    exit (98);
-			
-  /* while (*line != EOF) */
-  /* { */
+  (void)buff_size;
 
-  /* } */
+  write(STDOUT_FILENO, "$", 1); 
+  write(STDOUT_FILENO, " ", 1);   
+  while (1) 
+    {
+
+  /* linelen = getline(&lptr, &buff_size, stdin); */
+  /* if (lptr[linelen - 1] == EOF) */
+  /*   exit (98); */
+      inp_char = getchar();
+
+      if (inp_char != 10 || inp_char != EOF)
+	{
+	  line[linelen] = inp_char;
+	  linelen++;
+	  
+	}
+      else
+	{
+	  line[linelen] = '\0';
+	  printf("Llegamos aqui");
+	  return (lptr);
+	}
+}
   return (lptr);
 }
--- shell.c
+++ shell.c
@@ -21,10 +21,7 @@ int main(int ac, char **av)
 	}
 	else
 	{
-		while (1)
-		{
 		  line_read = read_line();
-		}
 	}
 	return (0);
 }
