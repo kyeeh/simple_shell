d7c1b7cf2220f105565cc5cee379b9535ddb8d79
nildiert <niljordan23@gmail.com>
2019-04-16 05:05:17 +0000

    [FIX] Fixed the file utils.c

--- utils.c
+++ utils.c
@@ -47,10 +47,6 @@ size_t _strlen(char *str)
 void print_char_pointer_arr(char *values[], size_t length)
 {
 	unsigned int i;
-
-	for (i = 0; i < length; i++)
-		printf("A[%u]: %s ", i, values[i]);
-	printf("\n");
 }
 
 /**
